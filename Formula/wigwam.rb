00b0b9563ac7e84aca52ebb8be236a2934ba6cf6
wigwam-release <release@wigwam.build>
2026-09-02 11:06:53 -0600

    Update wigwam to 0.5.1

--- Formula/wigwam.rb
+++ Formula/wigwam.rb
@@ -1,26 +1,26 @@
 class Wigwam < Formula
   desc "Access terminal sessions from the browser"
   homepage "https://github.com/Vennify/wigwam-releases"
-  version "0.5.0"
+  version "0.5.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://releases.wigwam.build/hut/v0.5.0/wigwam_darwin_arm64.tar.gz"
-      sha256 "d811fdcca9c40e7f3a9de6932da7ac624c2455901add7c35f9dcd27dfd046eed"
+      url "https://releases.wigwam.build/hut/v0.5.1/wigwam_darwin_arm64.tar.gz"
+      sha256 "17f1b8b99a9bbb937c797c6e1b9822932426df9c99da24969c2a007c378a98ad"
     else
-      url "https://releases.wigwam.build/hut/v0.5.0/wigwam_darwin_amd64.tar.gz"
-      sha256 "f9aa8f7a818a51b1b6052b1d9d48a634a280136bc8a8aaec6e36f0469a54ac3b"
+      url "https://releases.wigwam.build/hut/v0.5.1/wigwam_darwin_amd64.tar.gz"
+      sha256 "61c671513f60f1f9eb173aec11fc9cfd71cdf7211511e69764adec72fbd334d7"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://releases.wigwam.build/hut/v0.5.0/wigwam_linux_arm64.tar.gz"
-      sha256 "95f0969b270c51403d65dc382d032e59a5d6c8ab682684ab948dd58cc1771d38"
+      url "https://releases.wigwam.build/hut/v0.5.1/wigwam_linux_arm64.tar.gz"
+      sha256 "5210e94901ffe4503efb00f595b62fee1b1046072c5bb0e4baee389c5db5697e"
     else
-      url "https://releases.wigwam.build/hut/v0.5.0/wigwam_linux_amd64.tar.gz"
-      sha256 "acbd36262ef5241f100a96a95ba9e34b1c2da3143d1170a6adbe73f33b8f2b67"
+      url "https://releases.wigwam.build/hut/v0.5.1/wigwam_linux_amd64.tar.gz"
+      sha256 "a9686a37ca7c7e83bc31831313316e26e3646e79002b68f35b72964b5fcb234c"
     end
   end
 
